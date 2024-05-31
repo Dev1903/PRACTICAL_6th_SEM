da4ca43112aa7f68d8bad4146dd6e627ad03fc21
Dev1903 <101693082+Dev1903@users.noreply.github.com>
2024-05-31 20:50:45 +0530

    Create TCP_CLIENT.c

---
+++ COMPUTER_NETWORKS/1_WAY_MULTIPLE_TIME_USING_TCP_IP/TCP_CLIENT.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#define PORT 8090
+void main()
+  {
+  int clnsock_fd;
+  char message[1024];
+  struct sockaddr_in svraddr;
+  socklen_t svraddrlen=sizeof(struct sockaddr_in);
+  clnsock_fd=socket(AF_INET,SOCK_STREAM,0);
+  svraddr.sin_family=AF_INET;
+  svraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+  svraddr.sin_port=htons(PORT);
+  connect(clnsock_fd,(struct sockaddr*)&svraddr,svraddrlen);
+  while(1)
+  {
+    memset(message,0,sizeof(message));
+    printf("enter data for TCP/IP");
+    gets(message);
+    send(clnsock_fd,message,strlen(message),0);
+    if(strcmp(message,"exit")==0)break;
+  }
+}
