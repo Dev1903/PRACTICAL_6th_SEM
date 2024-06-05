882fd973433063d588dae398f692000168b43875
Dev1903 <101693082+Dev1903@users.noreply.github.com>
2024-06-05 23:06:05 +0530

    do changes

---
+++ COMPUTER_NETWORKS/TCP_IP/2_WAY_MULTIPLE_TIME/TCP_CLIENT.c
@@ -0,0 +1,47 @@
+#include <stdio.h>//clent
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[]) {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Exchange data
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        read(sock, buffer, BUFFER_SIZE);
+        printf("Server: %s", buffer);
+
+        printf("Client: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(sock, buffer, strlen(buffer), 0);
+    }
+    return 0;
+}
